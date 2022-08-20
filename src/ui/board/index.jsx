17f570d020a7f8d217539bdfddf0f0ce8b85541d
lee yahav <leeyahav018@gmail.com>
2022-08-20 21:55:43 +0300

    trs

--- src/ui/board/index.jsx
+++ src/ui/board/index.jsx
@@ -1,7 +1,7 @@
 import Card from "@/ui/card";
 export default function Board({ items, handleClick }) {
   return (
-    <div className="h-[40rem] w-[40rem] grid grid-cols-4 grid-rows-4">
+    <div className="2xl:h-[40rem] 2xl:w-[40rem] grid grid-cols-4 grid-rows-4 sm:w-[300px] sm:h-[300px]">
       {items.map((item, index) => {
         return (
           <Card key={index} id={index} item={item} handleClick={handleClick} />
